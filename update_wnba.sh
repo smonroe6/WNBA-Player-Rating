c10007e02ba141baaa60cac6dfa87c2a889bf9f8
Stephen Monroe <sdmonroe1999@gmail.com>
2025-09-09 18:01:05 -0500

    9-9-25 Update

---
+++ update_wnba.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Activate your virtual environment if needed
+# source /path/to/your/venv/bin/activate
+
+# Change to your project directory
+cd ~/WNBA-Player-Rating
+
+# Run notebooks (using papermill or jupyter nbconvert)
+jupyter nbconvert --to notebook --execute notebooks/CurrentSeason.ipynb --output notebooks/CurrentSeason.ipynb
+
+# Update the last_updated.txt file
+echo date -u +"%Y-%m-%dT%H:%M:%SZ" > data/Metrics/last_updated.txt
+
+# Git commands to push changes
+git add .
+git commit -m "Update $(date)"
+git push origin main

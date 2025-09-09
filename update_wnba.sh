58237e4001585b69151454ad5819d20618dd0a06
Stephen Monroe <sdmonroe1999@gmail.com>
2025-09-09 18:54:08 -0500

    Update Tue Sep  9 18:54:08 CDT 2025

--- update_wnba.sh
+++ update_wnba.sh
@@ -4,10 +4,13 @@
 # source /path/to/your/venv/bin/activate
 
 # Change to your project directory
-cd /Users/stephenmonroe/Desktop/Coding/WNBA-Player-Rating/
+cd /Users/stephenmonroe/Desktop/Coding/WNBA-Player-Rating/notebooks
 
 # Run notebooks (using papermill or jupyter nbconvert)
 python3 -m nbconvert --to notebook --execute CurrentSeason.ipynb --output executed_CurrentSeason.ipynb
+
+# Change to your project directory
+cd /Users/stephenmonroe/Desktop/Coding/WNBA-Player-Rating/
 
 # Update the last_updated.txt file
 date -u +"%Y-%m-%dT%H:%M:%SZ" > data/Metrics/last_updated.txt
